c3a23b83bdc1029372252f05d757a5884593eca6
Abiral Lamsal <abilamsal5@gmail.com>
2022-12-23 03:30:22 +0545

    Some change 2

--- src/stream.py
+++ src/stream.py
@@ -4,12 +4,12 @@ class WebStream():
     def __init__(self):
         self.matched=False
 
-    def camera(self,response):
+    async def camera(self,response):
         if not self.matched:
             response.send("Access denied")
             return
         stream=WebCamVideoStream()
         while self.matched:
             image=stream.read()
-            response.send(image.tobytes())
+            await response.send(image.tobytes())
         response.send("Stream Closed")
