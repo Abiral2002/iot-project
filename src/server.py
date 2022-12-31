58f269ffccae3a703f70b10bf4580fd826bb2b71
Abiral Lamsal <abilamsal5@gmail.com>
2022-12-31 17:27:26 +0545

    support 2

--- src/server.py
+++ src/server.py
@@ -16,7 +16,7 @@ class Password(BaseModel):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["http://192.168.1.73:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
